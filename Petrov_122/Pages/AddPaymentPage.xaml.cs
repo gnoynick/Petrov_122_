038c6959108b710e2ff9bcd9dd6c732f30522c87
0000 <0000@DESKTOP-4JMG02U>
2025-10-28 13:52:57 +0300

    обнова

--- Petrov_122/Pages/AddPaymentPage.xaml.cs
+++ Petrov_122/Pages/AddPaymentPage.xaml.cs
@@ -9,6 +9,7 @@ namespace Petrov_122.Pages
     public partial class AddPaymentPage : Page
     {
         private Payment _currentPayment = new Payment();
+
         public AddPaymentPage(Payment selectedPayment)
         {
             InitializeComponent();
@@ -20,6 +21,7 @@ namespace Petrov_122.Pages
 
             if (selectedPayment != null)
                 _currentPayment = selectedPayment;
+
             DataContext = _currentPayment;
         }
 
@@ -29,15 +31,30 @@ namespace Petrov_122.Pages
 
             if (string.IsNullOrWhiteSpace(_currentPayment.Name))
                 errors.AppendLine("Укажите название платежа!");
-            if (string.IsNullOrWhiteSpace(TBDateTime.Value?.ToString()))
-                errors.AppendLine("Укажите дату и время!");
-            if (string.IsNullOrWhiteSpace(_currentPayment.Num.ToString()))
+
+            // Проверка даты
+            if (string.IsNullOrWhiteSpace(TBDate.Text))
+                errors.AppendLine("Укажите дату!");
+            else
+            {
+                // Пытаемся преобразовать текст в дату
+                if (DateTime.TryParse(TBDate.Text, out DateTime parsedDate))
+                {
+                    _currentPayment.Date = parsedDate;
+                }
+                else
+                {
+                    errors.AppendLine("Укажите дату в правильном формате! (например: 15.12.2024)");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(_currentPayment.Num.ToString()) || _currentPayment.Num <= 0)
                 errors.AppendLine("Укажите количество!");
-            if (string.IsNullOrWhiteSpace(_currentPayment.Price.ToString()))
+            if (string.IsNullOrWhiteSpace(_currentPayment.Price.ToString()) || _currentPayment.Price <= 0)
                 errors.AppendLine("Укажите цену!");
-            if (string.IsNullOrWhiteSpace(_currentPayment.UserID.ToString()))
+            if (string.IsNullOrWhiteSpace(_currentPayment.UserID.ToString()) || _currentPayment.UserID <= 0)
                 errors.AppendLine("Укажите клиента!");
-            if (string.IsNullOrWhiteSpace(_currentPayment.CategoryID.ToString()))
+            if (string.IsNullOrWhiteSpace(_currentPayment.CategoryID.ToString()) || _currentPayment.CategoryID <= 0)
                 errors.AppendLine("Укажите категорию!");
 
             if (errors.Length > 0)
@@ -65,7 +82,7 @@ namespace Petrov_122.Pages
             TBPaymentName.Text = "";
             TBAmount.Text = "";
             TBCount.Text = "";
-            TBDateTime.Value = null;
+            TBDate.Text = "";
             CBCategory.SelectedIndex = -1;
             CBUser.SelectedIndex = -1;
         }
